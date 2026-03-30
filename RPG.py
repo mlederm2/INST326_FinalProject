b427891e58046bd012a3b9b933fdf7c2d9b75bbb
mlederm2 <mlederm2@terpmail.umd.edu>
2026-03-30 17:52:01 -0400

    Added a main file that will eventually contain all of the code

---
+++ RPG.py
@@ -0,0 +1,11 @@
+""" A RPG game that allows the player to do simple things like move, fight, and
+        use an inventory
+        
+
+"""
+
+
+class RPG:
+    """ the big class for the RPG game, full runner
+    """
+    
