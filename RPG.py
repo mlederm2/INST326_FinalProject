552e32f5c6b69df9b920213800a8d0138ec08e95
Jennifer Ruano <jenniferruano@jennifersmbp322.wireless.dyn.umd.edu>
2026-05-01 16:22:46 -0400

    Added map display

--- RPG.py
+++ RPG.py
@@ -306,65 +306,6 @@ class RPG:
         inventory.remove(item_name)
         print(f"'{item_name}' has been removed from your inventory.")
         return player_hp, enemy_frozen
-
-=======
-    
->>>>>>> 1a49229eb61d7e4bf440a3140eb08a46b7e75fac
-
-def move_player(
-    position: tuple[int, int], 
-    direction: str, 
-    game_map: list[list[str]]
-) -> tuple[int, int]:
-    """
-    An algorithm that controls how the player moves around the map.
-
-    It checks the direction the player wants to move and makes sure the move is
-    valid.
-    If the space is open and within the map, the player moves there. If not, 
-    the player stays in the same spot.
-
-    Args:
-        position: Current (row, column) of the player
-        direction: Direction to move ("up", "down", "left", "right")
-        game_map: 2D grid representing the map
-
-    Returns:
-        The new (row, column) position after attempting the move
-
-    Raises:
-        ValueError: If the direction is invalid.
-    """
-
-    row, col = position
-
-    moves = {
-        "up": (-1, 0),
-        "down": (1, 0),
-        "left": (0, -1),
-        "right": (0, 1)
-    }
-    # Validate direction
-    if direction not in moves:
-        raise ValueError("Invalid direction. Choose up, down, left, or right.")
-    
-    #Calculate new position
-    d_row, d_col = moves[direction]
-    new_row = row + d_row
-    new_col = col + d_col
-
-    # Check map boundaries 
-    if new_row < 0 or new_row >= len(game_map):
-        return position
-    if new_col < 0 or new_col >= len(game_map[0]): 
-        return position
-    #Check if the tile is blocked 
-    if game_map[new_row][new_col] == "#": 
-        return position
-    
-    #Move is valid
-    return (new_row, new_col)
-
     
 # Map setup
 map_data = [
